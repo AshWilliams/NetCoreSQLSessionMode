3874b70ff95abfef94947996bd2f9468fa5c399c
Robert Rozas Navarro <rorozasn@microsoft.com>
2019-01-02 21:27:21 -0300

    Sample .net core mvc for educ.ar, using sql server as key value cache, session manager

---
+++ CoreSessionMode/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CoreSessionMode.Models;
+//session cache
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text;
+
+namespace CoreSessionMode.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IDistributedCache cache;
+        public HomeController(IDistributedCache cache)
+        {
+            this.cache = cache;
+        }
+        public IActionResult Index()
+        {
+            //byte[] data = Encoding.UTF8.GetBytes("Hola mundo");
+            //cache.Set("timestamp",data);
+            cache.SetString("Mensaje", "Hola Mundo");            
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
